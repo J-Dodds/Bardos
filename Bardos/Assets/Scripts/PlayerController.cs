643e557ce2d5d13583d41fba28703b4a42315be8
J-Dodds <1004254@student.sae.edu.au>
2016-01-18 13:09:29 +1000

    Added Jumping functionality

--- Bardos/Assets/Scripts/PlayerController.cs
+++ Bardos/Assets/Scripts/PlayerController.cs
@@ -3,16 +3,27 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour 
 {
+	public float MovementSpeed = 100f;
+	public float JumpHeight = 1.0f;
+	public Rigidbody2D RB;
 
-	// Use this for initialization
-	void Start () 
-	{
-	
-	}
-	
+	private int JumpCount = 0;
+
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		if ( JumpCount == 0 && Input.GetButtonDown("Jump")) 
+		{
+			RB.AddForce(Vector3.up * JumpHeight);
+			JumpCount = 1;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag (Tags.Ground)) 
+		{
+			JumpCount = 0;
+		}
 	}
 }
