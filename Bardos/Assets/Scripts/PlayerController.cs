36f72f6124c0b8287a7a79a5c2e71b573e6382e4
J-Dodds <1004254@student.sae.edu.au>
2016-01-25 18:31:41 +1000

    Player movement implimented, Punching started

--- Bardos/Assets/Scripts/PlayerController.cs
+++ Bardos/Assets/Scripts/PlayerController.cs
@@ -3,20 +3,38 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour 
 {
-	public float MovementSpeed = 100f;
+	public float MovementSpeed = 500f;
 	public float JumpHeight = 1.0f;
 	public Rigidbody2D RB;
 
 	private int JumpCount = 0;
+	private int AttackCount = 0;
+	private Vector2 DirectionFacing;
+	private bool IsTouchingGround;
+	private bool IsAttacking;
 
 	// Update is called once per frame
 	void Update () 
 	{
-		if ( JumpCount == 0 && Input.GetButtonDown("Jump")) 
+		float MovementLeftRight = Input.GetAxis ("Horizontal") * MovementSpeed * Time.deltaTime;
+		DirectionFacing = new Vector2 (MovementLeftRight, 0);
+
+		if (JumpCount == 0 && Input.GetButtonDown("Jump")) 
 		{
-			RB.AddForce(Vector3.up * JumpHeight);
+			RB.AddForce(Vector2 * JumpHeight);
 			JumpCount = 1;
+			IsTouchingGround = false;
 		}
+
+		if (AttackCount == 0 && IsTouchingGround == true && Input.GetMouseButtonDown(0)) 
+		{
+			RB.AddForce (transform.forward * MovementSpeed);     
+		}
+	}
+
+	void FixedUpdate()
+	{
+		RB.AddForce (DirectionFacing);
 	}
 
 	void OnCollisionEnter2D(Collision2D other)
@@ -24,6 +42,7 @@ public class PlayerController : MonoBehaviour
 		if (other.gameObject.CompareTag (Tags.Ground)) 
 		{
 			JumpCount = 0;
+			IsTouchingGround = true;
 		}
 	}
 }
