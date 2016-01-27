11710ad7560fed52a922a0ee1cb9b53821e7b52b
J-Dodds <1004254@student.sae.edu.au>
2016-01-27 22:02:18 +1000

    Punching works perfectly

--- Bardos/Assets/Scripts/PlayerController.cs
+++ Bardos/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Diagnostics;
+using System;
 
 public class PlayerController : MonoBehaviour 
 {
@@ -8,53 +10,67 @@ public class PlayerController : MonoBehaviour
 	public Rigidbody2D RB;
 
 	private int JumpCount = 0;
-	private int AttackCount = 0;
-	private float Timer = 0f;
 	private Vector2 DirectionFacing;
 	private bool IsTouchingGround;
 	private bool IsAttacking;
+
+	//Coroutine that pauses value for X amount of seconds, then changes it
+	private IEnumerator AttackTimer()
+	{
+		IsAttacking = true;
+		yield return new WaitForSeconds (3);
+		IsAttacking = false;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		//Player movement on horizontal axis based on public float and Time.deltaTime
+		float MovementLeftRight = Input.GetAxis ("Horizontal") * MovementSpeed * Time.deltaTime;
+		DirectionFacing = new Vector2 (MovementLeftRight, 0);
+
+		//If JumpCount is 0 and jump button pressed, player jumps, increases jump count and sets IsTouchingGround bool to false
+		if (JumpCount == 0 && Input.GetButtonDown("Jump")) 
+		{
+			UnityEngine.Debug.Log ("Jumping");
+			RB.AddForce(Vector2.up * JumpHeight);
+			JumpCount = 1;
+			IsTouchingGround = false;
+			//Play jump animation
+			//Play jump sound
+		}
+
+		//If player hasnt attacked and is touching the ground on mouse button click, player dashes forwards, and AttackTimerRuns 
+		if (IsAttacking == false && IsTouchingGround == true && Input.GetMouseButtonDown(0)) 
+		{
+			UnityEngine.Debug.Log ("Punching");
+			RB.velocity = new Vector2 (MovementLeftRight, 0);
+			StartCoroutine(AttackTimer());
+			//Play punch animation
+			//Play punch sound
+		}
+	}
 
 	void FixedUpdate()
 	{
 		RB.AddForce (DirectionFacing);
 	}
 	
-	// Update is called once per frame
-	void Update () 
-	{
-		float MovementLeftRight = Input.GetAxis ("Horizontal") * MovementSpeed * Time.deltaTime;
-		DirectionFacing = new Vector2 (MovementLeftRight, 0);
-
-		if (JumpCount == 0 && Input.GetButtonDown("Jump")) 
-		{
-			Debug.Log ("Jumping");
-			RB.AddForce(Vector2.up * JumpHeight);
-			JumpCount = 1;
-			IsTouchingGround = false;
-		}
-
-		if (AttackCount == 0 && IsTouchingGround == true && Input.GetMouseButtonDown(0)) 
-		{
-			while(Timer < 1)
-			{
-				Timer = Timer + Time.deltaTime;
-				Debug.Log ("Punching");
-				RB.velocity = new Vector2 (MovementLeftRight, 0);
-				AttackCount = 1;
-			}
-
-			Debug.Log ("Punching Stopped");
-			AttackCount = 0;
-			Timer = 0;
-		}
-	}
-
 	void OnCollisionEnter2D(Collision2D other)
 	{
+		//On ground touch resets JumpCount and and IsTouchingGround
 		if (other.gameObject.CompareTag (Tags.Ground)) 
 		{
 			JumpCount = 0;
 			IsTouchingGround = true;
+			//Play landing sound		}
+
+		if (other.gameObject.CompareTag (Tags.Enemy) && IsAttacking == true) 
+		{
+			Destroy(other.gameObject);
+			//Play Enemy death animation
+			//Play on hit sound
+			//Play Enemy death sound
 		}
 	}
 }
