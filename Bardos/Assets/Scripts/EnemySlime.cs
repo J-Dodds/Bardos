f8f98dd82396f94dfa6ee61617721086f9dfa709
J-Dodds <1004254@student.sae.edu.au>
2016-01-27 22:01:46 +1000

    Added temporary enemy object

---
+++ Bardos/Assets/Scripts/EnemySlime.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySlime : MonoBehaviour 
+{
+	public int Health = 1;
+
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+}
